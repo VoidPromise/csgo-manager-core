e505f04324938ccdbbb9a44d005d0855bde362f3
Diego Migotto <diegomigotto@hotmail.com>
2023-03-04 00:45:21 -0300

    Adding initialization methods to csgo core

--- src/csgocore.cpp
+++ src/csgocore.cpp
@@ -1,2 +1,28 @@
-#include "pch.h"
 #include "csgocore.hpp"
+
+bool vp::csgocore::initialize() const noexcept
+{
+    return load_configuration() && load_resources();
+}
+
+bool vp::csgocore::load_state(
+    const std::filesystem::path& origin) const noexcept
+{
+    return false;
+}
+
+bool vp::csgocore::save_state(
+    const std::filesystem::path& destination) const noexcept
+{
+    return false;
+}
+
+bool vp::csgocore::load_configuration() const noexcept
+{
+    return false;
+}
+
+bool vp::csgocore::load_resources() const noexcept
+{
+    return false;
+}
--- src/csgocore.hpp
+++ src/csgocore.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <filesystem>
+
+#include "entt.hpp"
+
 namespace vp
 {
 class csgocore
@@ -19,11 +23,31 @@ class csgocore
         return instance;
     }
 
+    // Initialize all necessary resources.
+    [[nodiscard]] bool initialize() const noexcept;
+
+    // Load a saved state from filesystem,
+    [[nodiscard]] bool load_state(
+        const std::filesystem::path& origin) const noexcept;
+
+    // Save iternal state of the engine.
+    [[nodiscard]] bool save_state(
+        const std::filesystem::path& destination) const noexcept;
+
   private:
+    // Csgocore registry, holds all entities/components.
+    entt::registry _registry;
+
     // Initialize csgocore, no complicated processes.
     csgocore() = default;
 
     // We don't manage any resource, all should gracefully deallocate by itself.
     ~csgocore() = default;
+
+    // Load all configuration files and set internal parameters.
+    bool load_configuration() const noexcept;
+
+    // Load all csgocore relevant resource files, such as images and strings.
+    bool load_resources() const noexcept;
 };
 } // namespace vp
